94aedc752cd2e4e3fc2ef76f718c8bb1d6072d99
sriharsh1104 <prashantsriharsh1104@gmail.com>
2024-05-03 11:03:56 +0530

    Readme updated

--- README.md
+++ README.md
@@ -1,46 +1,50 @@
-# Getting Started with Create React App
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+# Solana Program Calling in React
 
-## Available Scripts
+This project demonstrates how to integrate Solana wallet functionality, particularly focusing on the Phantom wallet, into a React application. Deploy it on github than connect it to Vercel to Check the Linking . 
 
-In the project directory, you can run:
 
-### `npm start`
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+# Hi, I'm sriharsh! 👋
 
-The page will reload if you make edits.\
-You will also see any lint errors in the console.
 
-### `npm test`
+## 🚀 About Me
+I'm an enthusiastic software engineer with knowledge of the Ethereum blockchain, Solana, JavaScript, React, and algorithm.
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
 
-### `npm run build`
 
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
+## Run Locally
 
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
+Clone the project
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+```bash
+  git clone https://github.com/sriharsh1104/deeplinkphantom.git
+```
 
-### `npm run eject`
+Go to the project directory
 
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
+```bash
+  cd my-project
+```
 
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
+Install dependencies
 
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
+```bash
+  npm install -force
+```
 
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
+Start the server
 
-## Learn More
+```bash
+  npm start
+```
 
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
 
-To learn React, check out the [React documentation](https://reactjs.org/).
+## Feedback
+
+If you have any feedback, please reach out to us at prashantsriharsh1104@gmail.com
+
+
+## Support
+
+For support, email prashantsriharsh1104@gmail.com
--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
 import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
 import { WalletModalProvider, WalletMultiButton } from '@solana/wallet-adapter-react-ui';
-import { SolletWalletAdapter,PhantomWalletAdapter,TokenPocketWalletAdapter,  } from '@solana/wallet-adapter-wallets';
-import * as Linking from "expo-linking";
-
-import { SolanaMobileWalletAdapter,createDefaultAddressSelector,createDefaultAuthorizationResultCache,createDefaultWalletNotFoundHandler } from '@solana-mobile/wallet-adapter-mobile';
-
+import { SolletWalletAdapter, PhantomWalletAdapter, TokenPocketWalletAdapter } from '@solana/wallet-adapter-wallets';
+import {
+    SolanaMobileWalletAdapter,
+    createDefaultAddressSelector,
+    createDefaultAuthorizationResultCache,
+    createDefaultWalletNotFoundHandler,
+} from '@solana-mobile/wallet-adapter-mobile';
 import { clusterApiUrl } from '@solana/web3.js';
-import React, { FC, ReactNode, useMemo } from 'react';
+import { FC, ReactNode, useMemo } from 'react';
 
-// require('./App.css');
 require('@solana/wallet-adapter-react-ui/styles.css');
 
 const App: FC = () => {
@@ -22,9 +23,8 @@ const App: FC = () => {
 export default App;
 
 const deeplink = async () => {
-  const url = "https://phantom.app/ul/v1/connect";
-  window.open(url, "_self");
-
+    const url = 'https://phantom.app/ul/v1/connect';
+    window.open(url, '_self');
 };
 const Context: FC<{ children: ReactNode }> = ({ children }) => {
     // The network can be set to 'devnet', 'testnet', or 'mainnet-beta'.
@@ -34,24 +34,24 @@ const Context: FC<{ children: ReactNode }> = ({ children }) => {
     const endpoint = useMemo(() => clusterApiUrl(network), [network]);
 
     const wallets = useMemo(
-      () => [
-          new SolanaMobileWalletAdapter({
-              addressSelector: createDefaultAddressSelector(),
-              appIdentity: {
-                  name: 'phantomseparate',
-                  uri: 'https://phantom.app/ul/v1/connect',
-                  icon: 'relative/path/to/icon.png',
-              },
-              authorizationResultCache: createDefaultAuthorizationResultCache(),
-              chain: WalletAdapterNetwork.Devnet,
-              onWalletNotFound: createDefaultWalletNotFoundHandler(),
-          }),
-          new TokenPocketWalletAdapter(),
-          new PhantomWalletAdapter(),
-          new SolletWalletAdapter(),
-      ],
-      [],
-  );
+        () => [
+            new SolanaMobileWalletAdapter({
+                addressSelector: createDefaultAddressSelector(),
+                appIdentity: {
+                    name: 'phantomseparate',
+                    uri: 'https://phantom.app/ul/v1/connect',
+                    icon: 'relative/path/to/icon.png',
+                },
+                authorizationResultCache: createDefaultAuthorizationResultCache(),
+                chain: WalletAdapterNetwork.Devnet,
+                onWalletNotFound: createDefaultWalletNotFoundHandler(),
+            }),
+            new TokenPocketWalletAdapter(),
+            new PhantomWalletAdapter(),
+            new SolletWalletAdapter(),
+        ],
+        []
+    );
     return (
         <ConnectionProvider endpoint={endpoint}>
             <WalletProvider wallets={wallets} autoConnect>
